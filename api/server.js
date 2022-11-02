a91c7afbc9cf36353ff4b8f511b7d2193e1c4614
I G Borrelli <44072946+ItaloBorrelli@users.noreply.github.com>
2022-11-02 11:20:49 -0700

    [#17] Configure environment to use mongoose (#18)
    Co-authored-by: ItaloBorrelli <iborrelli@protonmail.com>

--- api/server.js
+++ api/server.js
@@ -1,33 +1,32 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
 
+const db = require("./db");
 // const <> = require("./routes/<>");
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
 // app.use("/api/<>", <>);
 
-const uri = process.env.DATABASE_URL;
-let client;
+let closeServer = () => {};
 
-const server = app.listen(3001, () => {
-  console.log("Started on PORT 3001");
-  client = new MongoClient(uri);
-  console.log("Client connected successfully");
-});
+db.once("open", () => {
+  const app = express();
 
-const closeServer = () => {
-  server.close(() => {
-    client.close();
-    console.log("Client disconnected successfully");
-    console.log("Process terminated");
+  app.use(cors());
+  app.use(express.json());
+
+  const server = app.listen(3001, () => {
+    console.log("Started on PORT 3001");
+    console.log("Client connected successfully");
   });
-};
+
+  closeServer = () => {
+    server.close(() => {
+      db.connection.close();
+      console.log("Client disconnected successfully");
+      console.log("Process terminated");
+    });
+  };
+});
 
 process.on("SIGTERM", () => {
   closeServer();
